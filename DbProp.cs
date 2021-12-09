6c9b09156e5a2b933ef551e53fa229a617846513
alcotico <mail-ral@yandex.ru>
2021-12-09 17:12:16 +0500

    Добавьте файлы проекта.

---
+++ DbProp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace TransportAutomatization
+{
+    class DbProp
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=tadb;");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed) connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open) connection.Close();
+        }
+
+        public MySqlConnection getConnection() => connection;
+    }
+}
